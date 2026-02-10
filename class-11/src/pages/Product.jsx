01f45cb74313434f2ea3f615220c37b44a35a9b8
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-02-10 21:30:38 +0530

    router

---
+++ class-11/src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+        <h1 className='text-3xl underline font-bold fixed  left-[50vw] -translate-x-1/2'>Product Page</h1>            
+    </div>
+  )
+}
+
+export default Product
