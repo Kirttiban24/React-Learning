b4d5d0ac4e024e5e866d865958afda45883cf9b0
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-02-10 19:39:22 +0530

    useeffect

---
+++ class-10/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [allPokemon, setallPokemon] = useState([])
+
+  const getData = async () => {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon')
+
+    setallPokemon(response.data.results)
+  } 
+  useEffect(function(){
+    getData()
+  },[])
+
+  return (
+    <div>
+      <button onClick={getData}>click</button>
+      {allPokemon.map(function(elem,idx){
+
+        return <h1 key={idx}>{elem.name}</h1>
+      })}
+    </div>
+  )
+}
+
+export default App
+App
