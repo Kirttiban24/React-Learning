0a1588c56cf8acb92916346d8839e33fc1b0652d
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-02-17 11:47:12 +0530

    context API

---
+++ class-13/src/components/Allcourses.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Course from './Course'
+
+const Allcourses = () => {
+  return (
+    <div>
+      Allcourses
+      <Course />
+      <Course />
+      <Course />
+      <Course />
+    </div>
+  )
+}
+
+export default Allcourses
---
+++ class-13/src/components/Section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Allcourses from './Allcourses'
+
+const Section1 = () => {
+  return (
+    <div>
+      Section2
+      <Allcourses />
+    </div>
+  )
+}
+
+export default Section1
